17ae74ede5b4efb302528509d6cbd434c1fe5f45
rajdeepnyk7-creator <rajdeepnyk7@gmail.com>
2026-02-24 12:44:42 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,38 @@
+let originalData = [];
 
+fetch("data/statement.json")
+    .then(response => response.json())
+    .then(data => {
+        originalData = data;
+        displayData(data);
+    });
+
+function displayData(data) {
+    const tableBody = document.getElementById("tableBody");
+    tableBody.innerHTML = "";
+
+    data.forEach(item => {
+        const row = `
+            <tr>
+                <td>${item.tp_no}</td>
+                <td>${item.fp_no}</td>
+                <td>${item.ward}</td>
+                <td>${item.purpose}</td>
+                <td>${item.area}</td>
+            </tr>
+        `;
+        tableBody.innerHTML += row;
+    });
+}
+
+function filterData() {
+    const tpValue = document.getElementById("tpSearch").value.toLowerCase();
+    const fpValue = document.getElementById("fpSearch").value.toLowerCase();
+
+    const filtered = originalData.filter(item =>
+        item.tp_no.toString().toLowerCase().includes(tpValue) &&
+        item.fp_no.toString().toLowerCase().includes(fpValue)
+    );
+
+    displayData(filtered);
+}
