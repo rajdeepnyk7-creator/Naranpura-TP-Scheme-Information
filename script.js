582901c86c8e58908a9653e81e9cb646f30fa51b
rajdeepnyk7-creator <rajdeepnyk7@gmail.com>
2026-02-26 14:34:14 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,38 @@
+let originalData = [];
+
 fetch("data/statement.json")
 .then(response => response.json())
 .then(data => {
 
-console.log(data[0]);
+originalData = data;
+
+console.log("HEADERS:");
+console.log(Object.keys(data[0]));
+
+displayData(data);
 
 });
+
+function displayData(data) {
+
+const tableBody = document.getElementById("tableBody");
+tableBody.innerHTML = "";
+
+data.forEach(item => {
+
+const keys = Object.keys(item);
+
+const row = `
+<tr>
+<td>${item[keys[3]]}</td>
+<td>${item[keys[6]]}</td>
+<td>${item[keys[2]]}</td>
+<td>${item[keys[7]]}</td>
+<td>${item[keys[8]]}</td>
+</tr>
+`;
+
+tableBody.innerHTML += row;
+
+});
+}
