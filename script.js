777375f16d9ee815a89e9ac103d690abab074e57
rajdeepnyk7-creator <rajdeepnyk7@gmail.com>
2026-02-26 14:25:34 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,46 +1,7 @@
-let originalData = [];
-
 fetch("data/statement.json")
 .then(response => response.json())
 .then(data => {
-    originalData = data;
-    displayData(data);
+
+console.log(data[0]);
+
 });
-
-function displayData(data) {
-
-    const tableBody = document.getElementById("tableBody");
-    tableBody.innerHTML = "";
-
-    data.forEach(item => {
-
-        const row = `
-        <tr>
-            <td>${item["T.P.S. NO."]}</td>
-            <td>${item["F.P. NO."]}</td>
-            <td>${item["Ward"]}</td>
-            <td>${item["PURPOSE"]}</td>
-            <td>${item["AREA IN SQ MT"]}</td>
-        </tr>
-        `;
-
-        tableBody.innerHTML += row;
-
-    });
-}
-
-function filterData() {
-
-const tpValue = document.getElementById("tpSearch").value.toLowerCase();
-const fpValue = document.getElementById("fpSearch").value.toLowerCase();
-
-const filtered = originalData.filter(item =>
-
-item["T.P.S. NO."].toString().toLowerCase().includes(tpValue) &&
-item["F.P. NO."].toString().toLowerCase().includes(fpValue)
-
-);
-
-displayData(filtered);
-
-}
